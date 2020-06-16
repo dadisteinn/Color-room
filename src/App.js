c130c9f50ab5d8d3a7e1d54d39c560ee3f1336e3
dadisteinn <dadisteinn@gmail.com>
2020-06-16 09:54:46 +0000

    added colorHelper

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import Palette from "./Palette";
 import seedPalettes from "./seedPalettes";
+import { generatePalette } from "./colorHelper";
 
 function App() {
+  console.log(generatePalette(seedPalettes[4]));
   return (
     <div className="App">
       <Palette {...seedPalettes[4]} />
