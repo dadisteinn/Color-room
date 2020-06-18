2d0d965ef84d3fd771927fa7a3a6f607c7cfefe5
dadisteinn <dadisteinn@gmail.com>
2020-06-18 11:53:50 +0000

    Added link and Route for 'More' btn in ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
 import "./ColorBox.css";
 
 export default class ColorBox extends Component {
@@ -30,6 +31,7 @@ export default class ColorBox extends Component {
 
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
+
             <p>{color}</p>
           </div>
 
@@ -41,7 +43,9 @@ export default class ColorBox extends Component {
             <button className="copy-btn">Copy</button>
           </div>
 
-          <span className="see-more">More</span>
+          <Link to="/" onClick={(e) => e.stopPropagation()}>
+            <span className="see-more">More</span>
+          </Link>
         </div>
       </CopyToClipboard>
     );
