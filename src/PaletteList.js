66875472a7c3b284455aaf8c4b6e6785a718bb83
dadisteinn <dadisteinn@gmail.com>
2020-06-18 11:05:44 +0000

    finished none responsive minipalette styling

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,7 +5,7 @@ import { withStyles } from "@material-ui/styles";
 const styles = {
   root: {
     backgroundColor: "blue",
-    height: "100%",
+    height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
@@ -35,6 +35,7 @@ const styles = {
 class PaletteList extends Component {
   render() {
     const { palettes, classes } = this.props;
+
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -43,7 +44,7 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
-              <MiniPalette {...palette} />
+              <MiniPalette {...palette} key={palette.id} />
             ))}
           </div>
         </div>
