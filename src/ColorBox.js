3e3814a448ede657417af765481c2aa0f3779412
Dadi Steinn <dadisteinn@gmail.com>
2020-06-26 21:00:00 +0000

    fixed navbar slider size and added classnames to colorbox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/ColorBoxStyles";
 
@@ -26,14 +27,16 @@ class ColorBox extends Component {
       <CopyToClipboard text={color} onCopy={this.changeCopyState}>
         <div className={classes.ColorBox} style={{ backgroundColor: color }}>
           <div
-            className={`${classes.copyOverlay} ${
-              copied ? classes.showOverlay : ""
-            }`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied,
+            })}
             style={{ backgroundColor: color }}
           />
 
           <div
-            className={`${classes.copyMsg} ${copied ? classes.showMsg : ""}`}
+            className={classNames(classes.copyMsg, {
+              [classes.showMsg]: copied,
+            })}
           >
             <h1>Copied!</h1>
 
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -26,6 +26,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
 
   openDialog(id) {
@@ -61,8 +62,7 @@ class PaletteList extends Component {
               <CSSTransition key={palette.id} classNames="fade" timeout={500}>
                 <MiniPalette
                   {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
-                  //handleDelete={deletePalette}
+                  goToPalette={this.goToPalette}
                   openDialog={this.openDialog}
                   id={palette.id}
                   key={palette.id}
