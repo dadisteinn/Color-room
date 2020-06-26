8b0b8214f98f0f58cc27e7cb42120023d86785f1
dadisteinn <dadisteinn@gmail.com>
2020-06-26 10:47:24 +0000

    made ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,11 +15,11 @@ export default {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: (props) => (props.showingFullPalette ? "20%" : "10%"),
+      height: (props) => (props.showingFullPalette ? "20%" : "33.3333%"),
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: (props) => (props.showingFullPalette ? "10%" : "10%"),
+      height: (props) => (props.showingFullPalette ? "10%" : "20%"),
     },
     [sizes.down("xs")]: {
       width: "100%",
@@ -115,6 +115,9 @@ export default {
       textShadow: "1px 2px black",
       textTransform: "uppercase",
       background: "rgba(255, 255, 255, 0.2)",
+      [sizes.down("xs")]: {
+        fontSize: "6rem",
+      },
     },
     "& p": {
       fontSize: "2rem",
