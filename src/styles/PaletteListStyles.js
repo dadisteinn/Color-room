226c1326328945c27fe84adef4a72fa04fa49135
dadisteinn <dadisteinn@gmail.com>
2020-06-26 14:24:12 +0000

    added bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,10 +1,20 @@
+import bg from "./bg-svg.svg";
+import sizes from "./sizes";
+
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "flex-start",
+    /* backgrund by SVGBackground.com */
+    backgroundColor: "#eb3d30",
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
+    overflow: "auto",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -12,6 +22,15 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "65%",
+    },
+    [sizes.down("lg")]: {
+      width: "80%",
+    },
+    [sizes.down("lg")]: {
+      width: "70%",
+    },
   },
   nav: {
     display: "flex",
@@ -28,6 +47,12 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "1.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+    },
   },
 };
